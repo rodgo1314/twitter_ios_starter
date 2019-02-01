6bcb1428e1c6d8c26f55804cae7791a5f94cc784
Rodrigo leyva <leyvarodrigo36@yahoo.com>
2019-01-31 22:46:54 -0800

    finished app

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Rodrigo Leyva on 1/31/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
